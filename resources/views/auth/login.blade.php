0e950888a9e659103ae3622ba9924979b092aec5
dungcavang <xuandung026@gmail.comm>
2025-06-07 23:00:56 +0700

    them authentication

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    ok
-</body>
-</html>
+@extends('layouts.app')
+@section('content')
+    <div class="container-fluid">
+        <div class="row d-flex justify-content-center align-items-center h-100 my-5">
+            <div class="col-md-9 col-lg-6 col-xl-5">
+                <img src="./assets/images/banners/login.png" class="img-fluid" alt="Sample image">
+            </div>
+            <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+                <form method="POST" action="{{ route('loginPost') }}">
+                    @csrf
+                    <div class="form-outline mb-4">
+                        <label class="font-weight-bold" for="username">Login name</label>
+                        <input required type="text" id="username" class="form-control form-control-lg" name="username"
+                            placeholder="Enter username" />
+                    </div>
+
+                    <div class="form-outline mb-3">
+                        <label class="font-weight-bold" for="password">Password</label>
+                        <input required type="password" id="password" class="form-control form-control-lg" name="password"
+                            placeholder="Enter password" />
+                    </div>
+                    <p class="text-center text-danger"><strong><?php if (isset($alert))
+        echo $alert?></strong></p>
+                    <div class="text-center text-lg-start mt-4 pt-2">
+                        <input type="submit" class="btn btn-primary btn-lg" name="login"
+                            style="padding-left: 2.5rem; padding-right: 2.5rem;" value="Đăng nhập">
+                        <p class="small font-weight-bold mt-2 pt-1 mb-0">"Không có tài khoản ?"
+                            <a href="{{ route('register') }}" class="text-danger">Đăng Ký</a>
+                        </p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
