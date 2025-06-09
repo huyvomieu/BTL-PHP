837cb0358dd4913ae9ba5fe5f84e6ed156c0728b
huyvomieu <huy040424@gmail.com>
2025-06-09 19:23:40 +0700

    add: insert bản ghi vào các order và order_details sau khi thanh toán song

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use DB;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\User;
 use App\Models\CartDetail;
+use App\Models\Order;
 class OrderController extends Controller
 {
     public function confirm(Request $request, $id)
@@ -35,17 +37,20 @@ class OrderController extends Controller
     }
     public function processPayment(Request $request)
     {
+        $user_id = session('user_id', '12');
+        $totalPrice = (new CartDetail())->getTotalPrice($user_id);
         $method = $request->query('method');
         if ($method == 'vnpay') {
             $order_code = rand(1, 10000);
             $vnp_TxnRef = $order_code; //Mã giao dịch thanh toán tham chiếu của merchant
-            $vnp_Amount = 1000000; // Số tiền thanh toán
+            $vnp_Amount = $totalPrice * 100; // Số tiền thanh toán
             $vnp_BankCode = 'NCB'; //Mã phương thức thanh toán
 
             $vnp_TmnCode = config('vnpay.vnp_tmncode');
             $vnp_HashSecret = config('vnpay.vnp_hashsecret');
             $vnp_Url = config('vnpay.vnp_url');
-            $vnp_Returnurl = config('vnpay.vnp_returnurl');;
+            $vnp_Returnurl = config('vnpay.vnp_returnurl');
+            ;
 
             //Expire
             date_default_timezone_set('Asia/Ho_Chi_Minh');
@@ -86,56 +91,61 @@ class OrderController extends Controller
             }
 
             $vnp_Url = $vnp_Url . "?" . $query;
-            $vnpSecureHash =   hash_hmac('sha512', $hashdata, $vnp_HashSecret);//  
-			$vnp_Url .= 'vnp_SecureHash=' . $vnpSecureHash;
+            $vnpSecureHash = hash_hmac('sha512', $hashdata, $vnp_HashSecret);//  
+            $vnp_Url .= 'vnp_SecureHash=' . $vnpSecureHash;
             // lưu đơn hàng trước khi chuyển hướng
-            // if (isset($vnp_HashSecret)) {
-            //     $user_id = session('user_id', '12');  // Mặc định là 12 vì chưa làm phần login, logout
-            //     $user = User::find($user_id);
-            //     $order_created_time = date("Y-m-d H:i:s");
-            //     $order_receiver = $user->user_fullname;
-            //     $order_address = $user->user_address;
-            //     $order_value = 100000;
-            //     $order_phone = $user->user_phone;
-            //     $order_notes = "";
-
-            //     $vnpSecureHash = hash_hmac('sha512', $hashdata, $vnp_HashSecret);
-            //     $vnp_Url .= 'vnp_SecureHash=' . $vnpSecureHash;
-            //     $order_payment = 'vnpay';
-
-            //     // $sql_insert_invoice = "INSERT INTO 
-            //     // tblorder(user_id,order_created_time,order_address,order_value,order_phone,order_receiver,order_payment, order_code) 
-            //     // VALUES('" . $user_id . "','" . $order_created_time . "','" . $order_address . "','" . $order_value . "','" . $order_phone . "', '" . $order_receiver . "', '" . $order_payment . "', '$order_code')";
-            //     //     $insert_invoice_result = mysqli_query($mysqli, $sql_insert_invoice);
-            //     //     $order_id = mysqli_insert_id($mysqli);
-            //     //     $_SESSION['order_id'] = $order_id;
-            //     //     $sql_cart = "SELECT * FROM tblcart_details 
-            //     // where tblcart_details.cart_id = $cart_id";
-            //     //     $query_cart = mysqli_query($mysqli, $sql_cart);
-            //     //     while ($row = mysqli_fetch_assoc($query_cart)) {
-            //     //         $product_id = $row['product_id'];
-            //     //         $quantity = $row['quantity'];
-            //     //         $sql_product = "SELECT * FROM tblproduct
-            //     // 	where product_id = $product_id";
-            //     //         $query_product = mysqli_query($mysqli, $sql_product);
-            //     //         $row_product = mysqli_fetch_assoc($query_product);
-            //     //         $purchase_price = $row_product['product_price'] * (100 - $row_product['product_discount']) / 100;
-            //     //         $sql_insert_order_detail = "INSERT INTO tblorder_details (order_id, product_id, quantity, order_code, purchase_price) VALUES ('$order_id', '$product_id', '$quantity', '$order_code', '$purchase_price')";
-            //     //         $insert_detail_result = mysqli_query($mysqli, $sql_insert_order_detail);
-            //     //         $sql_update = "UPDATE tblproduct set product_quantity = product_quantity - $quantity where product_id = $product_id";
-            //     //         $query_update = mysqli_query($mysqli, $sql_update);
-            //     //     }
-            //     //     $id_delete_cart = $_SESSION['cart_id'];
-            //     //     $sql_delete_all_products = "DELETE FROM tblcart_details WHERE cart_id = $id_delete_cart";
-            //     //     $delete_result = mysqli_query($mysqli, $sql_delete_all_products);
-            //     //     unset($_SESSION['total_value']);
-            // }
-            return redirect()->away($vnp_Url);
+            if (isset($vnp_HashSecret)) {
+                return redirect()->away($vnp_Url);
+            } else {
+                echo 'Lỗi!';
+            }
         }
     }
-    public function returnVnpay() {
+    public function returnVnpay(Request $request)
+    {
+        $user_id = session('user_id', '12');  // Mặc định là 12 vì chưa làm phần login, logout
+        $user = User::find($user_id);
+        $order_created_time = date("Y-m-d H:i:s");
+        $order_receiver = $user->user_fullname;
+        $order_address = $user->user_address;
+        $order_value = $request->query('vnp_Amount') / 100;
+        $order_phone = $user->user_phone;
+        $order_notes = "test";
+        $order_payment = 'vnpay';
+        $order_code = $request->query('vnp_TxnRef');
+        // INSERT BẢN GHI VÀO BẢNG ORDER
+        $order_id =  Order::insertGetId([
+            'user_id' => $user_id,
+            'order_created_time' => $order_created_time,
+            'order_address' => $order_address,
+            'order_notes' => $order_notes,
+            'order_value' => $order_value,
+            'order_phone' => $order_phone,
+            'order_status' => 1,
+            'order_receiver' => $order_receiver,
+            'order_payment' => $order_payment,
+            'order_code' => $order_code,
+        ]);
+        // DUYỆT INSERT BẢN GHI VÀO BẢNG ORDER_DETAILS
+        $products = (new CartDetail())->getListCartById($user_id);
+        $data = $products->map(function ($product) use ($order_id, $order_code) {
+            return [
+                'order_id' => $order_id,
+                'product_id' => $product->product_id,
+                'quantity' => $product->quantity, // mặc định số lượng
+                'order_code' => $order_code,
+                'purchase_price' => $product->product_price,
+            ];
+        })->toArray();
+        DB::table('tblorder_details')->insert($data);
+        // XÓA GIỎ HÀNG SAU KHI THANH TOÁN SONG
+
+        // GIẢM SỐ LƯỢNG PRODUCT 
+        
         return view('order.finish');
-    }     public function history() {
+    }
+    public function history()
+    {
         return view('order.history');
     }
 }
